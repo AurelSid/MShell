8f96a6f0e846e36d6071897a9290570c746b4aa2
Aurelio Sideris <asideris@mi-c2-p6.s19.be>
2024-09-20 17:10:07 +0200

    s

--- objs/command.d
+++
@@ -1,2 +0,0 @@
-objs/command.o: src/command.c src/../include/minishell.h \
-  include/libft/get_next_line/get_next_line.h include/libft/libft.h
--- objs/init_data.d
+++
@@ -1,2 +0,0 @@
-objs/init_data.o: src/init_data.c src/../include/minishell.h \
-  include/libft/get_next_line/get_next_line.h include/libft/libft.h
