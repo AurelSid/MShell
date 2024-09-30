202fc9e410e4fb60e0e5e703e8f86af9c9349e55
Aurelio Sideris <asideris@shi-r12-p1.s19.be>
2024-09-30 13:40:18 +0200

    s

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: asideris <asideris@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 13:34:08 by roko              #+#    #+#             */
-/*   Updated: 2024/09/25 16:04:36 by asideris         ###   ########.fr       */
+/*   Updated: 2024/09/30 13:37:49 by asideris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,24 +28,32 @@ int	main(int argc, char **argv, char **env)
 	ft_init_data(&data);
 	ft_env_copy(env, &data);
 	rl = readline("$> ");
+	// while (1)
+	// {
 	data.input = rl;
 	ft_tokens_fill_list(&data);
-	ft_print_tokens_list(data);
+	// ft_print_tokens_list(data);
 	ft_commands_fill_list(&data);
 	ft_check_all_access(&data);
-	ft_print_commands(data);
 	tmp_cmd = data.command_top;
 	while (tmp_cmd)
 	{
 		ft_apply_redir(tmp_cmd);
+		ft_check_built_ins(tmp_cmd,&data);
 		if (tmp_cmd->next != NULL)
 		{
+			fprintf(stderr, "\n\n-- executing [%s] pipe --\n\n", tmp_cmd->name);
 			ft_exec_pipe(tmp_cmd, env);
 		}
 		else
+		{
+			fprintf(stderr, "\n\n-- executing [%s]  --\n\n", tmp_cmd->name);
 			ft_exec_cmd(tmp_cmd, env);
+		}
+		fprintf(stderr, "\n-- [%s] OK --\n\n", tmp_cmd->name);
 		tmp_cmd = tmp_cmd->next;
+		// }
+		// list_open_file_descriptors();
 	}
 	return (0);
 }
-// echo "Hello World" | wc - c
