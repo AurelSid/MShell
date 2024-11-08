6bbbf4a54a405f0ae2c4caeb1f31522cc4e5c09a
Aurelio Sideris <asideris@mi-r3-p2.s19.be>
2024-11-08 15:43:38 +0100

    s

--- objs/split_args.d
+++
@@ -1,9 +0,0 @@
-objs/split_args.o: src/split_args.c src/../include/minishell.h \
-  include/libft/get_next_line/get_next_line.h include/libft/libft.h \
-  /Users/vpelc/.brew/opt/readline/include/readline/history.h \
-  /Users/vpelc/.brew/opt/readline/include/readline/rlstdc.h \
-  /Users/vpelc/.brew/opt/readline/include/readline/rltypedefs.h \
-  /Users/vpelc/.brew/opt/readline/include/readline/readline.h \
-  /Users/vpelc/.brew/opt/readline/include/readline/keymaps.h \
-  /Users/vpelc/.brew/opt/readline/include/readline/chardefs.h \
-  /Users/vpelc/.brew/opt/readline/include/readline/tilde.h
